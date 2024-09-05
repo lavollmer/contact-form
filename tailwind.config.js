9273ade3fab85427cfb7985ebbe8b22d540fae57
Laura <laura.e.vollmer@gmail.com>
2024-09-05 10:11:46 -0500

    added design colors in tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-lighter": "hsl(148, 38%, 91%)",
+        "green-medium": "hsl(169, 82%, 27%)",
+        "red-design": "hsl(0, 66%, 54%)",
+        "grey-medium": "hsl(186, 15%, 59%)",
+        "grey-dark": "hsl(187, 24%, 22%)",
+      },
+    },
   },
   plugins: [],
-}
+};
