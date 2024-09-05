b0712833e5916321de872a3f268e72e4550bfbfa
Laura <laura.e.vollmer@gmail.com>
2024-09-05 10:45:52 -0500

    updating flexbox styling of query type

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
           {/* first line of the form */}
           <div>
             <form className="flex flex-col">
+              {/* input boxes with reusable box */}
               <div className="flex flex-row space-x-10 items-center">
                 <Information title="First Name" value={userInfo} />
                 <Information title="Last Name" value={userInfo} />
@@ -22,15 +23,36 @@ function App() {
               <div>
                 <Information title="Email Address" value={userInfo} />
               </div>
+              {/* Query type */}
+              <div className="flex flex-col">
+                <div>
+                  <h2>Query Type</h2>
+                </div>
+                <div className="flex flex-row justify-center items-center">
+                  <div className="border border-grey-medium rounded-lg">
+                    <label>General Enquiry</label>
+                    <input type="checkbox" className="circle-checkbox" />
+                  </div>
+                  <div className="border border-grey-medium rounded-lg">
+                    <label>Support Request</label>
+                    <input type="checkbox" className="circle-checkbox" />
+                  </div>
+                </div>
+              </div>
               <div>
                 <Information title="General Message" value={userInfo} />
               </div>
+              {/* consent checkbox */}
               <div>
                 <input type="checkbox" id="consent" name="consent" />
                 <label htmlFor="consent" className="text-grey-dark">
                   I hereby consent to being contacted by the team
                 </label>
               </div>
+              {/* submit button */}
+              <button className="bg-green-medium rounded-lg py-2 px-8">
+                <span className="text-white">Submit</span>
+              </button>
             </form>
           </div>
         </div>
