13d8f6eba0dc5621f24d32c205a66d4d18a21692
Laura <laura.e.vollmer@gmail.com>
2024-09-05 10:29:12 -0500

    creating the first form input box

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,26 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="screen font-karla">
+      <div className="screen">
         <div className="boxy">
           <div>
             <h1 className="text-grey-dark font-bold text-4xl">Contact Us</h1>
+          </div>
+          {/* first line of the form */}
+          <div>
+            <div>
+              <form className="flex flex-row justify-between items-center">
+                <div className="flex flex-col justify-start space-y-2">
+                  <label className="text-md">First Name</label>
+                  <input type="text" id="text" className="border border-grey-medium rounded-lg" />
+                </div>
+                <div>
+                  <label>Last Name</label>
+                  <input type="text" id="text" />
+                </div>
+              </form>
+            </div>
+            <div></div>
           </div>
         </div>
       </div>
