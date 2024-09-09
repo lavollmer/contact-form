c8b859fb1e251aba55dc9fa415bee420b82f7028
Laura <laura.e.vollmer@gmail.com>
2024-09-09 14:24:51 -0500

    adding mobile classes in app.css for entire app

--- src/App.jsx
+++ src/App.jsx
@@ -52,10 +52,10 @@ function App() {
 
   return (
     <>
-      <div className="screen">
-        <div className="boxy">
+      <div className="screenMobile md:screen">
+        <div className="boxyMobile md:boxy">
           <div>
-            <h1 className="text-grey-dark font-bold text-4xl">Contact Us</h1>
+            <h1 className="text-grey-dark font-bold text-xl md:text-4xl">Contact Us</h1>
           </div>
           {/* first line of the form */}
           <div className="flex flex-col pt-6 w-full">
@@ -94,11 +94,11 @@ function App() {
                 {/* Query type */}
                 <div className="flex flex-col space-y-4">
                   <div>
-                    <h2 className="text-lg">Query Type *</h2>
+                    <h2 className="text-sm md:text-lg">Query Type *</h2>
                   </div>
                   <div className="flex flex-row space-x-4 text-lg">
                     <div className="flex flex-row justify-center items-center space-x-8 border border-grey-medium rounded-lg py-2 px-4 cursor-pointer hover:border-green-medium hover:bg-green-lighter hover:bg-opacity-80 hover:shadow-lg w-full">
-                      <div className="flex items-center p-2 space-x-4">
+                      <div className="flex items-center p-2 space-x-4 text-sm md:text-lg">
                         <input
                           type="checkbox"
                           className="custom-checkbox"
@@ -111,7 +111,7 @@ function App() {
                       </div>
                     </div>
                     <div className="flex flex-row justify-center items-center space-x-4 border border-grey-medium rounded-lg py-2 px-4 cursor-pointer hover:border-green-medium hover:bg-green-lighter hover:bg-opacity-80 hover:shadow-lg w-full">
-                      <div className="flex items-center space-x-4">
+                      <div className="flex items-center space-x-4 text-sm md:text-lg">
                         <input
                           type="checkbox"
                           className="custom-checkbox"
@@ -127,13 +127,13 @@ function App() {
                 </div>
                 <div>
                   <div className="flex flex-col space-y-2">
-                    <label className="text-lg">General Message *</label>
+                    <label className="text-sm md:text-lg">General Message *</label>
                     <input
                       type="text"
                       id="text"
                       value={message}
                       onChange={(e) => setMessage(e.target.value)}
-                      className="border border-grey-medium rounded-lg py-6 px-4 text-lg cursor-pointer hover:border-green-medium hover:shadow-lg"
+                      className="border border-grey-medium rounded-lg py-6 px-4 text-sm md:text-lg cursor-pointer hover:border-green-medium hover:shadow-lg"
                     />
                     {errors.message && (
                       <div className="text-red-500">{errors.message}</div>
@@ -148,14 +148,14 @@ function App() {
                     name="consent"
                     className="custom-checkbox"
                   />
-                  <label htmlFor="consent" className="text-grey-dark">
+                  <label htmlFor="consent" className="text-grey-dark text-sm md:text-lg">
                     I consent to the processing of my personal data
                   </label>
                 </div>
                 {/* submit button */}
                 <div className="flex flex-row items-center justify-center w-full h-full">
-                  <button className="bg-green-medium rounded-lg w-full h-full p-4 cursor-pointer hover:border-green-lighter hover:shadow-lg">
-                    <span className="text-white text-lg">Submit</span>
+                  <button className="bg-green-medium rounded-lg w-full h-full p-2 md:p-4 cursor-pointer hover:border-green-lighter hover:shadow-lg">
+                    <span className="text-white text-sm md:text-lg">Submit</span>
                   </button>
                 </div>
               </form>
--- src/components/inputBox.jsx
+++ src/components/inputBox.jsx
@@ -4,12 +4,12 @@ const inputBox = ({ title, value, onChange, error }) => {
   return (
     <div>
       <div className="flex flex-col space-y-2 w-full">
-        <label className="text-lg">{title} *</label>
+        <label className="text-sm md:text-lg">{title} *</label>
         <input
           type="text"
           value={value}
           onChange={onChange}
-          className="border border-grey-medium rounded-lg p-2 py-2 w-full text-lg cursor-pointer hover:border-green-medium hover:shadow-lg"
+          className="border border-grey-medium rounded-lg p-2 py-2 w-full text-sm md:text-lg cursor-pointer hover:border-green-medium hover:shadow-lg"
         />
         {error && <div className="text-red-500">{error}</div>}
       </div>
